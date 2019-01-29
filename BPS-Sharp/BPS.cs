d546fb07b2995ec517b6e93f67ccfb723f25f551
GlitchyPSIX <supermglitchy3@gmail.com>
2019-01-29 00:38:49 -0400

    Literally everything, working

--- BPS-Sharp/BPS.cs
+++ BPS-Sharp/BPS.cs
@@ -9,122 +9,118 @@ using System.Threading.Tasks;
     C# port by GlitchyPSI */
 namespace BPS_Sharp
 {
-    static class BPS
+    static public class BPS
     {
 
-        static public async Task<byte[]> TryBPS(byte[] ROM, byte[] Patch, string path)
+        static public async Task TryBPS(string ROM, string Patch, string path)
         {
-            byte[] ret;
-            ret = await ApplyBPS(ROM, Patch);
-            File.WriteAllBytes(path, ret);
-            return ret;
+            await Task.Run(() => { File.WriteAllBytes(path, ApplyBPS(File.ReadAllBytes(ROM), File.ReadAllBytes(Patch))); });
         }
 
-        static async Task<byte[]> ApplyBPS(byte[] ROM, byte[] Patch)
+        static byte[] ApplyBPS(byte[] ROM, byte[] Patch)
         {
-            return await Task.Run(() =>
+            Crc32 crc32 = new Crc32();
+            long patchpos = 0;
+            int u8() { return Patch[patchpos++]; }
+            int u32at(int pos) { return (Patch[pos + 0] << 0 | Patch[pos + 1] << 8 | Patch[pos + 2] << 16 | Patch[pos + 3] << 24) >> 0; }
+
+            long decode()
             {
-                Crc32 crc32 = new Crc32();
-                long patchpos = 0;
-                int u8() { return Patch[patchpos++]; }
-                int u32at(int pos) { return (Patch[pos + 0] << 0 | Patch[pos + 1] << 8 | Patch[pos + 2] << 16 | Patch[pos + 3] << 24) >> 0; }
-
-                long decode()
+                var ret = 0;
+                var sh = 0;
+                while (true)
                 {
-                    int ret = 0;
-                    int sh = 0;
-                    while (true)
-                    {
-                        int next = u8();
-                        ret += (next ^ 0x80) << sh;
-                        if ((next & 0x80) == 128) return ret;
-                        sh += 7;
-                    }
+                    var next = u8();
+                    ret += (next ^ 0x80) << sh;
+                    if ((next & 0x80) == 128) return ret;
+                    sh += 7;
                 }
+            }
 
-                long decodes()
+            long decodes()
+            {
+                long enc = decode();
+                long ret = enc >> 1;
+                if ((enc & 1) != 0)
                 {
-                    long enc = decode();
-                    long ret = enc >> 1;
-                    if ((enc & 1) != 0)
-                    {
-                        ret = -ret;
-                    }
-                    return ret;
+                    ret = -ret;
                 }
+                return ret;
+            }
 
-                if (u8() != 0x42 || u8() != 0x50 || u8() != 0x53 || u8() != 0x31)
-                {
-                    throw new WrongPatchException();
-                }
-                if (decode() != ROM.Length)
-                {
-                    throw new WrongFileException(decode(), ROM.Length);
-                }
-                if (crc32.ComputeChecksum(ROM) != u32at(Patch.Length - 12))
-                {
-                    throw new WrongFileException();
-                }
+            if (u8() != 0x42 || u8() != 0x50 || u8() != 0x53 || u8() != 0x31)
+            {
+                throw new WrongPatchException();
+            }
+            if (decode() != ROM.Length)
+            {
+                throw new WrongFileException(decode(), ROM.Length);
+            }
+            if (crc32.ComputeChecksum(ROM) != u32at(Patch.Length - 12))
+            {
+                throw new WrongFileException();
+            }
 
-                byte[] _out = new byte[decode()];
-                int outpos = 0;
+            byte[] _out = new byte[decode()];
+            int outpos = 0;
 
-                switch (outpos)
+            switch (outpos)
+            {
+                case (0):
+                    break;
+            }
+
+            long metalen = decode();
+            patchpos += metalen;
+            long inreadpos = 0;
+            long outreadpos = 0;
+            while (patchpos < Patch.Length - 12)
+            {
+                long thisinstr = decode();
+                long len = (thisinstr >> 2) + 1;
+                int action = (int)(thisinstr & 3);
+                switch (action)
                 {
                     case (0):
+                        {
+                            for (int i = 0; i < len; i++)
+                            {
+                                _out[outpos] = ROM[outpos];
+                                outpos++;
+                            }
+                        }
                         break;
-                }
-                long inreadpos = 0;
-                long outreadpos = 0;
-                while (patchpos < Patch.Length - 12)
-                {
-                    long thisinstr = decode();
-                    long len = (thisinstr >> 2) + 1;
-                    int action = (int)(thisinstr & 3);
-                    switch (action)
-                    {
-                        case (0):
+                    case (1):
+                        {
+                            for (int z = 0; z < len; z++)
                             {
-                                for (int i = 0; i < len; i++)
-                                {
-                                    _out[outpos] = ROM[outpos];
-                                    outpos++;
-                                }
+                                _out[outpos++] = (byte)u8();
                             }
-                            break;
-                        case (1):
-                            {
-                                for (int i = 0; i < len; i++)
-                                {
-                                    _out[outpos++] = (byte)u8();
-                                }
-                            }
-                            break;
-                        case (2):
-                            {
+                        }
+                        break;
+                    case (2):
+                        {
 
-                                inreadpos += decodes();
-                                for (int i = 0; i < len; i++)
-                                {
-                                    _out[outpos++] = ROM[inreadpos++];
-                                }
-                            }
-                            break;
-                        case (3):
+                            inreadpos += decodes();
+                            for (int w = 0; w < len; w++)
                             {
-
-                                outreadpos += decodes();
-                                for (int i = 0; i < len; i++)
-                                {
-                                    _out[outpos++] = _out[outreadpos++];
-                                }
+                                _out[outpos++] = ROM[inreadpos++];
                             }
-                            break;
-                    };
-                }
-                return _out;
-            });
+                        }
+                        break;
+                    case (3):
+                        {
 
+                            outreadpos += decodes();
+                            for (var ax = 0; ax < len; ax++)
+                            {
+                                _out[outpos++] = _out[outreadpos++];
+                            }
+                        }
+                        break;
+                };
+            }
+            return _out;
         }
 
         class WrongFileException : Exception
@@ -140,6 +136,11 @@ namespace BPS_Sharp
             public WrongFileException() : base("Wrong file. Expected ROMsize of " + romsize + ", got " + decsize)
             {
             }
+
+            public override string ToString()
+            {
+                return base.Message;
+            }
         }
 
         class WrongPatchException : Exception
@@ -147,6 +148,10 @@ namespace BPS_Sharp
             public WrongPatchException() : base("The specified patch file isn't a BPS patch.")
             {
 
+            }
+            public override string ToString()
+            {
+                return base.Message;
             }
         }
 
---
+++ Tester 1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using BPS_Sharp;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tester_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rom = "";
+            string patch = "";
+            string path = "";
+
+            Console.WriteLine("BPS-Sharp (Just patcher.) - Ported from Alcaro's \"CPS\" JS-based BPS patcher." +
+                "\nAsynch is the future, somebody said in a Discord guild. They weren't wrong." +
+                "\n-GlitchyPSI");
+
+            do
+            {
+                Console.WriteLine("Insert the path to the base ROM file.");
+                rom = Console.ReadLine();
+            }
+            while (!File.Exists(rom));
+            do
+            {
+                Console.WriteLine("Insert the path to the patch for this ROM file.");
+                patch = Console.ReadLine();
+            }
+            while (!File.Exists(patch));
+            Console.WriteLine("Insert the path where the patched ROM is going to be saved to.");
+            path = Console.ReadLine();
+            if (File.Exists(path))
+            {
+                Console.WriteLine("Oh, hold on... this file already exists, and will be overwritten.");
+                Console.ReadKey(true);
+                }
+            TryPatch(rom, patch, path).Wait();
+        }
+
+        static async Task TryPatch(string rom, string patch, string path)
+        {
+            try
+            {
+                await BPS.TryBPS(rom, patch, path);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("We're done here.");
+                Console.ReadKey();
+            }
+        }
+        }
+    }
